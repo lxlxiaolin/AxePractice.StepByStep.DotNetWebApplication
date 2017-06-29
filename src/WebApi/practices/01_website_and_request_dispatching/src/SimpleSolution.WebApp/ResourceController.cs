777b9992b85486a6a55727f45d917b21301a27d6
bojian <bjchen@thoughtworks.com>
2017-06-29 19:24:09 +0800

    rewrite controller

---
+++ src/WebApi/practices/01_website_and_request_dispatching/src/SimpleSolution.WebApp/ResourceController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleSolution.WebApp
+{
+    public class ResourceController : ApiController
+    {
+        [AcceptVerbs("GET", "PUT", "POST", "DELETE")]
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ src/WebApi/practices/01_website_and_request_dispatching/src/SimpleSolution.WebApp/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleSolution.WebApp
+{
+    public class UsersController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Get(string userId)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new {message=$"{userId}"});
+        }
+
+        [HttpGet]
+        public HttpResponseMessage AnotherGet(string userId)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new {message=$"{userId}"});
+        }
+    }
+}
